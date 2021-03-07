f8cd39e972ca2ad95bfff65d52a7081e745fdacb
Mariya Ivanova <m80ivanova@gmail.com>
2021-03-07 15:44:42 -0600

    About Component, Contact Component,Icons, Favorites, and Comments

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
-import { Card } from "react-native-elements";
+import { Text, View, ScrollView, FlatList } from "react-native";
+import { Card, Icon } from "react-native-elements";
 import { CAMPSITES } from "../shared/campsites";
+import { COMMENTS } from "../shared/comments";
 
 //From the props we will pull the campsite object and send it another component - the RenderCampsite.
-//The RenderCampsite component will receive in it's parameter list the property "campsite" from the destructured campsite object it erecived as props
+//The RenderCampsite component will receive in it's parameter list the property "campsite" from the destructured campsite object it recieved as props. Later on we needed addl props so just added props in the parameter list so we can receive all of them. Then inside the function we still destructured the {campsite} prop
 // with if(campsite) we are checking if campsites is null or undefined-if it is-it will return false, otherwsie it'll be truthy.
 //The Card component from react-native-elements lets us use a featuredTitle prop-which value will be the campsite name. The image prop again will use the require() function with the path to the image
 //The Text component from react-native is also rendered inside the Card component and we can use style prop to style it. Since this is an object we again need to use the 2 sets of {}. Style might look like CSS but it is not. This is JS.
 //If the if statement returns as falsy we will just return an empty <View/> tag simillar to an empty <div>
+//In the RenderComments functional component we will return a FlatList inside the Card as the FlatList expects an array and we are providing the comments array as its data. For tne renderItem prop we will give a function name renderCommentItem
+//Since all the comments have an unique id we can set the keyExtractor prop of the FlatList to use that id for the unique key required for every item in the array.
+//The renderCommentItem function automatically gets item as part of the props so we are destructuring it. We are using templete literal for the author and date of the comment in order to be able to use some dashes and a comma in the text that will be displayed
+//The boolean favorite property in the state of the Campsite info component is set up there for now and it will store if the  selected campsite has been marked as a favorite or not. It is initialized to false and an event handler (markFavorite) has been set up to toggle it to true
+//The the markFavorite event handler and the favorite state data has to passed as props in the rendering of the RenderCampsite component at the bottom
+//The name prop of the Icon component is set up to check if it has received from props the favorite property as true or false. If true-that means the heart icon will be displayed in solid color, if false-it will just be outlined.
 
-function RenderCampsite({campsite}) {
+function RenderCampsite(props) {
+
+    const {campsite} = props;
     if (campsite) {
         return (
             <Card 
@@ -19,6 +28,14 @@ function RenderCampsite({campsite}) {
                 <Text style={{margin: 10}}>
                     {campsite.description}
                 </Text>
+                <Icon 
+                    name={props.favorite ? "heart" : "heart-o"}
+                    type="font-awesome"
+                    color="#f50"
+                    raised
+                    reverse
+                    onPress={() => props.favorite? console.log("Already set as a favorite") : props.markFavorite()}
+                />
             </Card>
 
         );
@@ -27,12 +44,42 @@ function RenderCampsite({campsite}) {
 }
 
 
+function RenderComments({comments}) {
+    
+        const renderCommentItem = ({item}) => {
+            return (
+              <View style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.text}</Text>
+                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+                <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
+              </View>
+            );
+        };
+
+        return (
+            <Card title="Comments">
+                <FlatList 
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+
+            </Card>
+        );
+}
+
 class CampsiteInfo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS,
+            favorite: false
         };
+    }
+
+    markFavorite() {
+        this.setState({favorite: true})
     }
 
     static navigationOptions = {
@@ -42,7 +89,16 @@ class CampsiteInfo extends Component {
     render() {
         const campsiteId = this.props.navigation.getParam("campsiteId");
         const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
-        return <RenderCampsite campsite={campsite} />;
+        const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId);
+        return (
+          <ScrollView>
+            <RenderCampsite campsite={campsite} 
+                favorite={this.state.favorite}
+                markFavorite={() => this.markFavorite()}
+            />
+            <RenderComments comments={comments} />
+          </ScrollView>
+        );
     }
 }
 
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,66 +2,105 @@ import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
-import { View, Platform} from "react-native";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
+
+import { View, Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 
-//The createStackNavigator is a function which will store in the DirectoryNavigator. The function has 1 required argument called the routeConfig object.This object will hold the components which will be part of the navigation stack.
+//The createStackNavigator is a function which return value we will store in the DirectoryNavigator. The function has 1 required argument called the routeConfig object.This object will hold the components which will be part of the navigation stack.
 //The second argument of the createStackNavigator function is optional. The initialRoutName-sets the default view of whichever component we want displyed first when the app loads.
 //The defaultNavigationOptions object will help to configure the settings for the header.These are built-in settings, not custom ones
 //We will be passing the createStackNavigator(or rather tha variable DirectoryNavigator which holds it's return value) to the next function we imported-createAppContainer. We will assign the return value of createAppContainer to a variable named AppNavigator. The createAppContainer will return our top-level Navigator Component and connect it to the React-Native environment. Most often we will have to wrapp our top navigator component in a createAppContainer like this.
 
 const DirectoryNavigator = createStackNavigator(
-    {
-        Directory: { screen: Directory },
-        CampsiteInfo: { screen:CampsiteInfo }
+  {
+    Directory: { screen: Directory },
+    CampsiteInfo: { screen: CampsiteInfo },
+  },
+  {
+    initialRouteName: "Directory",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
     },
-    {
-        initialRouteName: "Directory",
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: "#5637DD"
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-                color: "#fff"
-            }
-        }
-    }
+  }
 );
 
 const HomeNavigator = createStackNavigator(
-    {
-        Home: { screen: Home},
-        
+  {
+    Home: { screen: Home },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
     },
-    {
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: "#5637DD"
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-                color: "#fff"
-            }
-        }
-    }
+  }
+);
+
+const AboutNavigator = createStackNavigator(
+  {
+    About: { screen: About },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const ContactNavigator = createStackNavigator(
+  {
+    Contact: { screen: Contact },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
 );
 
 //Creating the drawer navigator. The createDrawerNavigator function will have the first argument be an object that contains the screens(views) that will be in the drawer
 //We want to route through the stack navigator, that is why we are setting up the screens to render the HomeNavigator and the DirectoryNavigator instead of just the Home and the Directory components.
 //In the optional second argument for addl configuration we can set the drawer's background color
 
+
 const MainNavigator = createDrawerNavigator(
-    {
-        Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator }
-    },
-    {
-        drawerBackgroundColor: "#CEC8FF"
-    }
-)
+  {
+    Home: { screen: HomeNavigator },
+    Directory: { screen: DirectoryNavigator },
+    About: { screen: AboutNavigator },
+    Contact: { screen: ContactNavigator },
+  },
+  {
+    drawerBackgroundColor: "#CEC8FF",
+  }
+);
 
 const AppNavigator = createAppContainer(MainNavigator);
 
@@ -72,44 +111,43 @@ const AppNavigator = createAppContainer(MainNavigator);
 //Filter method always returns an array so we will grab the first item with index 0 by adding- [0]
 
 class Main extends Component {
-    //constructor(props) {
-       // super(props);
-       // this.state = {
-         //   campsites: CAMPSITES,
-           // selectedCampsite: null
-        //};
-    //}
+  //constructor(props) {
+  // super(props);
+  // this.state = {
+  //   campsites: CAMPSITES,
+  // selectedCampsite: null
+  //};
+  //}
 
-    //onCampsiteSelect(campsiteId) {                       //This was comennted out from here as we will have this be handled now in the Directory component
-        //this.setState({selectedCampsite: campsiteId})
-    //}
+  //onCampsiteSelect(campsiteId) {                       //This was comennted out from here as we will have this be handled now in the Directory component
+  //this.setState({selectedCampsite: campsiteId})
+  //}
 
-    //The Directory and the CampsiteInfo components rendered below are replaced by the AppNavigator variable which now holds the container that holds the DirectoryNavigator, which in it's turn holds both the screens for the Directory and the CampsiteInfo components.
-    //The paddingTop property of the style object will need to utilize the Platform API we imported from react-native. That will help us set some conditions to check if the devise is IOS or not using the ternary operator. The reason is that on different devices the padding will render differently so we need to be able to handle both cases.
-    //If the Platform Operating System is "ios" the padding will be 0, if not, we will use the value given to us by Expo which sets the statusBarHight dinamically
+  //The Directory and the CampsiteInfo components rendered below are replaced by the AppNavigator variable which now holds the container that holds the DirectoryNavigator, which in it's turn holds both the screens for the Directory and the CampsiteInfo components.
+  //The paddingTop property of the style object will need to utilize the Platform API we imported from react-native. That will help us set some conditions to check if the devise is IOS or not using the ternary operator. The reason is that on different devices the padding will render differently so we need to be able to handle both cases.
+  //If the Platform Operating System is "ios" the padding will be 0, if not, we will use the value given to us by Expo which sets the statusBarHight dinamically
 
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          paddingTop:
+            Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+        }}
+      >
+        <AppNavigator />
 
-    render() {
-        return (
-            <View style={{
-                    flex: 1,
-                    paddingTop: Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight
-                }}
-            >
-
-                <AppNavigator />
-
-               {/* <Directory 
+        {/* <Directory 
                     campsites={this.state.campsites} 
                     onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
                 />
                 <CampsiteInfo
                     campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}
                />*/}
-
-            </View>
-        );
-    }
+      </View>
+    );
+  }
 }
 
 export default Main;
