16997102bfaec69fa3bc40cb9bf2d254cbd04ce3
Mariya Ivanova <m80ivanova@gmail.com>
2021-03-14 18:26:57 -0500

    Swipe and Delete Favorites

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-import { FlatList, View, Text } from "react-native";
+import { FlatList, View, Text, StyleSheet } from "react-native";
 import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { SwipeRow } from "react-native-swipe-list-view";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { deleteFavorite } from "../redux/ActionCreators";
 
 //This component will displaying only the favorited campsites. In it's return method we are rendering a FlatList.
 //In the FlatList we are using the data prop to access the campsites part of the whole campsites object. Then we need to filter through that to find
@@ -11,12 +14,19 @@ import { baseUrl } from "../shared/baseUrl";
 //For every campsite object we'll check if the favorites array includes() the id of that campsite.
 //The filter method will return a new array that consists of all the campsites with ids that found a match in tne favorites array.
 //In the keyExtractor prop we will pass each item into a function that will extract the id from each item and then turn that id(which is a number) to a string. That way we have a unique key to use for each item.
+//The SwipeRow component will surround the ListItem as we want to unable the ListItem to be swiped to have the additional functionallity become visible.
+//The rightOpneValue prop of the SwipeRow determines that the user can swipe from the right. It takes a negative whole number which represents the number of pixels which the user will have to swipe from right to left for this row to open.
+//The SwipeRow expects 2 View components after that. The first View will hold the things that stay hidden until the user swipes. The second View will hold the things that are shown on the screen by default before the user swipes.
 
 const mapStateToProps = state => {
     return {
         campsites: state.campsites,
         favorites: state.favorites
     };
+};
+
+const mapDispatchToProps = {
+    deleteFavorite: campsiteId => deleteFavorite(campsiteId)
 };
 
 class Favorites extends Component {
@@ -29,12 +39,26 @@ class Favorites extends Component {
         const { navigate } = this.props.navigation;
         const renderFavoriteItem = ({item}) => {
             return (
-                <ListItem
+              <SwipeRow rightOpenValue={-100} style={styles.swipeRow}>
+                <View style={styles.deleteView}>
+                    <TouchableOpacity
+                        style={styles.deleteTouchable}
+                        onPress={() => this.props.deleteFavorite(item.id)}
+                    >
+                        <Text style={styles.deleteText}>Delete</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                  <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                    onPress={() => navigate("CampsiteInfo", {campsiteId: item.id})}
-                />
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
+                    onPress={() =>
+                      navigate("CampsiteInfo", { campsiteId: item.id })
+                    }
+                  />
+                </View>
+              </SwipeRow>
             );
         };
 
@@ -61,5 +85,26 @@ class Favorites extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+    deleteView: {
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        flex: 1
+    },
+    deleteTouchable: {
+        backgroundColor: "red",
+        height: "100%",
+        justifyContent: "center"
+    },
+    deleteText: {
+        color: "white",
+        fontWeight: "700",
+        textAlign: "center",
+        fontSize: 16,
+        width: 100
+    }
+})
 
-export default connect(mapStateToProps)(Favorites);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
--- redux/favorites.js
+++ redux/favorites.js
@@ -6,6 +6,9 @@ import * as ActionTypes from "./ActionTypes";
 //First we need to check if that campsite id already exists in the state's array by using the first if statement with the array method "includes".
 //The includes method takes a single argument, in this case the action.payload and checks to see if it matches a single item in the array. If so, ti will return with a boolean value of true, otherwise it will be false.
 //If true it will return the previous state as nothing needs to change, if false-it'll return a new state with a new campsite id concatenated to the end of the state's array.
+//In the DELETE_FAVORITE case we are filtering through the favorites array from the state. The filter method will return a new array that includes all the favorite items that do not match the action.payload(which included all the Ids for the campsites).
+//That way we get a new array that includes all the favorites without the ones we have deleted. And we can return that as the new state.
+
 
 export const favorites = (state = [], action) => {
     switch (action.type) {
@@ -14,6 +17,9 @@ export const favorites = (state = [], action) => {
                 return state;
             }
             return state.concat(action.payload);
+
+            case ActionTypes.DELETE_FAVORITE:
+                return state.filter(favorite => favorite !== action.payload);
 
             default:
                 return state;
