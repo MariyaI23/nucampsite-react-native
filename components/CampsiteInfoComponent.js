a643be9d171bed20c2dbac9229bec5d310671d63
Mariya Ivanova <m80ivanova@gmail.com>
2021-03-20 19:03:51 -0500

    More Alerts, Animations, Gestures

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -43,6 +43,8 @@ function RenderCampsite(props) {
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
+
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderGrant: () => {
@@ -68,11 +70,12 @@ function RenderCampsite(props) {
               ],
               { cancelable: false }
             );
-          }
-
-          return true;
+          } else if(recognizeComment(gestureState)) {
+           props.onShowModal()
+            }
+        return true;
       }
-    });
+    })
 
     if (campsite) {
         return (
